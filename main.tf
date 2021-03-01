d3eedc451313218f8e0cc95fac3f44423391e147
Sébastien Braun <sebbraun@gmail.com>
2021-03-01 17:57:01 +0100

    patched vars

--- main.tf
+++ main.tf
@@ -10,51 +10,46 @@ required_version = ">= 0.14.0"
 
 # Configure the OpenStack Provider
 provider "openstack" {
-  user_name   = "$user_name"
-  tenant_id = "$tenant_id"
-  password    = "$password"
-  auth_url    = "$auth_url"
-  region      = "$region"
+  user_name   = var.user_name
+  tenant_id   = var.tenant_id
+  password    = var.password
+  auth_url    = var.auth_url
+  region      = var.region
 }
 
 #resource "openstack_blockstorage_volume_v2" "vol-01" {
-#  name = "$volume_name_01"
+#  name = var.volume_name_01
 #  size = 1
 #}
 
 #resource "openstack_blockstorage_volume_v2" "vol-02" {
-#  name = "$volume_name_02"
+#  name = var.volume_name_02
 #  size = 1
 #}
 
 resource "openstack_compute_keypair_v2" "yet-keypair" {
   name       = "yet-keypair"
-  public_key = "$keypair"
+  public_key = var.keypair
 }
 
 resource "openstack_compute_instance_v2" "myinstance" {
-  name            = "$instance_name"
-  image_id        = "$image_id"
-  flavor_id       = "$flavor_id"
+  name            = var.instance_name
+  image_id        = var.image_id
+  flavor_id       = var.flavor_id
   key_pair        = "yet-keypair"
   security_groups = ["default"]
 
   network {
-    name = "$network"
+    name = var.network
   }
 }
 
 #resource "openstack_compute_volume_attach_v2" "attached" {
-#  instance_id = "${openstack_compute_instance_v2.myinstance.id}"
-#  volume_id   = "${openstack_blockstorage_volume_v2.volume-01.id}"
+#  instance_id = var.openstack_compute_instance_v2.myinstance.id
+#  volume_id   = var.openstack_blockstorage_volume_v2.volume-01.id
 #}
 
 #resource "openstack_compute_volume_attach_v2" "attached-01" {
-#  instance_id = "${openstack_compute_instance_v2.myinstance.id}"
-#  volume_id   = "${openstack_blockstorage_volume_v2.vol-01.id}"
-#}
-
-#resource "openstack_compute_volume_attach_v2" "attached-02" {
-#  instance_id = "${openstack_compute_instance_v2.myinstance.id}"
-#  volume_id   = "${openstack_blockstorage_volume_v2.vol-02.id}"
+#  instance_id = var.openstack_compute_instance_v2.myinstance.id
+#  volume_id   = var.openstack_blockstorage_volume_v2.vol-01.id
 #}
